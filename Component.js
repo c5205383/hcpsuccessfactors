cb6be5ff8b564e6c01f6e48a9c1422ff7a73fd90
c5205383 <c5205383@users.noreply.github.com>
2016-03-25 16:48:21 +0800

    Added files via upload

---
+++ Component.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"hcpsuccessfactors/model/models"
+], function(UIComponent, Device, models) {
+	"use strict";
+
+	return UIComponent.extend("hcpsuccessfactors.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function() {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+			var testCompyList = new sap.ui.model.odata.ODataModel("/destinations/testCompyList/refapp-companylist-web/companylist.svc/", true);
+    		this.setModel(testCompyList,"testCompyList");
+		}
+	});
+
+});
