2414ca5b78508d01e30bfd18614f91a5c5ac2ba7
Zero Yu <zero.yu@sap.com>
2016-04-07 05:39:47 +0000

    update basic view and controller of detail002

--- webapp/controller/detail/detail002.controller.js
+++ webapp/controller/detail/detail002.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"hcpsuccessfactors/model/models"
-], function(Controller, models) {
+	"hcpsuccessfactors/model/models",
+    "sap/ui/model/json/JSONModel"
+], function(Controller, models, JSONModel) {
 	"use strict";
 
 	return Controller.extend("hcpsuccessfactors.controller.detail.detail002", {
@@ -14,8 +15,133 @@ sap.ui.define([
 		onInit: function() {
 			// set the device model
 			this.getView().setModel(models.createDeviceModel(), "device").bindElement("device>/");
-		}
+			this.createDetailView();
+		},
 
+		createDetailView: function() {
+			var page2=this.getView().byId("detail2page");
+			var _this=this;
+			$.ajax({
+				url: "https://middlewarei326962trial.hanatrial.ondemand.com/hcpmiddleware/api/getGoalPlanTemplate",
+				type:"GET",
+				dataType: "jsonp",
+				data: {
+					json: "processResults"
+				},
+				jsonp: "callback",
+				jsonpCallback: "processResults",
+				async: true,
+				success: function(data){
+					var templateModel=new JSONModel(data);
+					_this.getView().setModel(templateModel,"templateModel");
+					var selectItem="";
+					var selectItemArray=[];
+					for(var i=0;i<data.list.length;i++){
+						selectItem=new sap.ui.core.Item({
+							key:data.list[i].id,
+							text:data.list[i].name
+						});
+						selectItemArray.push(selectItem);
+					}
+					var templateSelect=sap.m.Select({
+						id:"tSelect",
+						autoAdjustWidth: true,
+						selectKey:data.list[0].id,
+						items:selectItemArray
+					}).attachChange(function(oEvent){
+						_this.onTempalteChanged(oEvent);
+					},this);
+					page2.addContent(templateSelect);
+					
+					var curId=sap.ui.getCore().byId("tSelect").getItemAt(0).getKey();
+					var callBack="g"+curId;
+					$.ajax({
+						url: "https://middlewarei326962trial.hanatrial.ondemand.com/hcpmiddleware/api/getGoal",
+						type:"GET",
+						dataType: "jsonp",
+						data: {
+							id: curId
+						},
+						jsonp: "callback",
+						jsonpCallback: callBack,
+						success: function(gdata){
+							var goalModel=new JSONModel(gdata);
+							_this.getView().setModel(goalModel,"goalModel");
+							var goalTable=new sap.m.Table({
+								id:"gTable",
+								columns:[
+								         new sap.m.Column({
+								        	 header:new sap.m.Text({text:"id"})
+								         }),
+								         new sap.m.Column({
+								        	 header:new sap.m.Text({text:"name"})
+								         }),
+								         new sap.m.Column({
+								       	   	 header:new sap.m.Text({text:"userId"})
+								         }),
+								         new sap.m.Column({
+								       	   	 header:new sap.m.Text({text:"state"})
+								         })
+								],
+								items:{
+								       path: 'goalModel>/goals',
+									       factory : function (sId, oContext) {
+										       return new sap.m.ColumnListItem({
+										    	   cells: [
+							                                new sap.m.Text({
+							                                	text:"{goalModel>id}"
+							                                }),
+							                                new sap.m.Text({
+							                                	text:"{goalModel>name}"
+							                                }),	
+							                                new sap.m.Text({
+							                                	text:"{goalModel>userId}"
+							                                }),
+							                                new sap.m.Text({
+							                                	text:"{goalModel>state}"
+							                                })
+							                        ]
+										       }); 
+									       }
+								}    
+							});
+							page2.addContent(goalTable);
+						},
+						error: function(){
+							alert("fail");
+						}
+					});
+					
+				},
+				error: function(){
+					alert("fail");
+				}
+			});
+			
+				
+		},
+		
+		onTempalteChanged: function(){
+			alert("change");
+			this.getView().getModel("goalModel").setData({});
+			var curId=sap.ui.getCore().byId("tSelect").getSelectedKey();
+			var callBack="g"+curId;
+			var _this=this;
+			$.ajax({
+				url: "https://middlewarei326962trial.hanatrial.ondemand.com/hcpmiddleware/api/getGoal",
+				type:"GET",
+				dataType: "jsonp",
+				data: {
+					id: curId
+				},
+				jsonp: "callback",
+				jsonpCallback: callBack,
+				success: function(gdata){
+					_this.getView().getModel("goalModel").setData(gdata);
+				}
+			});
+		}
+		
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
