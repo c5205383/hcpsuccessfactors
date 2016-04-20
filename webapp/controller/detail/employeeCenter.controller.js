99897c1adad511ea65c94a8d7a983abff859b5bd
zerosrat <zero@zerosrat.com>
2016-04-20 03:06:56 +0000

    employee center

--- webapp/controller/detail/employeeCenter.controller.js
+++ webapp/controller/detail/employeeCenter.controller.js
@@ -1,10 +1,26 @@
 sap.ui.define([
-	"hcpsuccessfactors/controller/BaseController"
-], function(BaseController) {
+	"hcpsuccessfactors/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function(BaseController, JSONModel) {
 	"use strict";
 
 	return BaseController.extend("hcpsuccessfactors.controller.detail.employeeCenter", {
-
+		onInit: function(){
+			var isDebug=true;
+			if(!isDebug){
+				//
+			}else{
+				this.bindMockData();
+			}
+		},
+		
+		bindMockData: function(){
+			var usersModelPath = jQuery.sap.getModulePath("hcpsuccessfactors", "/mockData/users.json");
+			var usersModel = new JSONModel();
+			usersModel.loadData(usersModelPath, null, false);
+			this.getView().setModel(usersModel, "UsersModel");
+		}
+		
 	});
 
 });
