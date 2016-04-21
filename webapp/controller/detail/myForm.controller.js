2160e057526647ce7df3a50aa369e88d0650f60b
Huayi Zou <huayi.zou@sap.com>
2016-04-21 07:19:02 +0000

    huayi

--- webapp/controller/detail/myForm.controller.js
+++ webapp/controller/detail/myForm.controller.js
@@ -1,9 +1,24 @@
 sap.ui.define([
-	"hcpsuccessfactors/controller/BaseController"
-], function(BaseController) {
+	"hcpsuccessfactors/controller/BaseController",
+	"jquery.sap.global",
+	"sap/ui/core/mvc/Controller",
+	"sap/m/Popover",
+	"sap/m/Button"
+], function(BaseController, jQuery, Controller, Popover, Button) {
 	"use strict";
-
 	return BaseController.extend("hcpsuccessfactors.controller.detail.myForm", {
+
+		onInit: function() {
+
+		},
+
+		onCollapseExapandPress: function(event) {
+			var navigationList = this.getView().byId("navigationList");
+			var expanded = !navigationList.getExpanded();
+
+			navigationList.setExpanded(expanded);
+			navigationList.setWidth(expanded ? "20rem" : "3rem");
+		}
 
 	});
 
