99b5bb694d2b487fa3eaf165912ba2c64d9860f6
Huayi Zou <huayi.zou@sap.com>
2016-04-25 10:15:13 +0000

    0425 formdetail

---
+++ webapp/controller/detail/form/formdetail.controller.js
@@ -0,0 +1,10 @@
+jQuery.sap.require("hcpsuccessfactors.util.format");
+sap.ui.define([
+   "hcpsuccessfactors/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function (BaseController, JSONModel) {
+    "use strict";
+    return BaseController.extend("hcpsuccessfactors.controller.detail.form.formdetail", {
+    	
+    });
+});
