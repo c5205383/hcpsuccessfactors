3173427be404b8db98b4a259733b3a50ef29fa5c
Ling Zuo <ling.zuo@sap.com>
2016-04-29 08:39:09 +0000

    changed by Ling: promotion process view, controller, style.css, i18n.properties.

--- webapp/controller/detail/starbucks/PromotionProcess.controller.js
+++ webapp/controller/detail/starbucks/PromotionProcess.controller.js
@@ -1,9 +1,41 @@
 sap.ui.define([
-	"hcpsuccessfactors/controller/BaseController"
+	"hcpsuccessfactors/controller/BaseController",
+	"jquery.sap.global",
+	"sap/m/Button",
+	"sap/m/MessageToast",
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
 ], function(BaseController) {
 	"use strict";
 
 	return BaseController.extend("hcpsuccessfactors.controller.detail.starbucks.PromotionProcess", {
+		onInit: function() {
+			this.onButtonPressed();
+			this.onButtonPressedData();
+		},
+
+		onButtonPressed: function() {
+			var oLayout = this.byId("PromotionProcess");
+			if (!oLayout.getVisible()) {
+				oLayout.setVisible(true);
+			} else {
+				oLayout.setVisible(false);
+			}
+		},
+		onButtonPressedData: function() {
+			var oLayout = this.byId("DispayPromotionData");
+			if (oLayout.getVisible()) {
+				oLayout.setVisible(false);
+			} else {
+				oLayout.setVisible(true);
+			}
+		},
+		redoPromotion: function() {
+
+		},
+		addPromotion: function() {
+
+		}
 
 	});
 
