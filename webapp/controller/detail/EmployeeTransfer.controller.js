b063da7639700c18e97935fe03fc99b5a21c6305
zerosrat <zero@zerosrat.com>
2016-04-29 03:00:03 +0000

    employee transfer bind data

--- webapp/controller/detail/EmployeeTransfer.controller.js
+++ webapp/controller/detail/EmployeeTransfer.controller.js
@@ -1,10 +1,41 @@
 sap.ui.define([
-	"hcpsuccessfactors/controller/BaseController"
-], function(BaseController) {
+	"hcpsuccessfactors/controller/BaseController",
+	"sap/ui/model/json/JSONModel"
+], function(BaseController, JSONModel) {
 	"use strict";
 
 	return BaseController.extend("hcpsuccessfactors.controller.detail.EmployeeTransfer", {
-
+        init: function(){
+            var isDebug=true;
+            if(!isDebug){
+                this.bindData();
+            }else{
+                this.bindMockData();
+            }
+        },
+        
+        bindData: function(){
+            var _this = this;
+			$.ajax({
+				url: "/sfsfdataservice/hcp/getUserDirectReports",
+				type: "GET",
+				async: true,
+				success: function(drdata) {
+				    var directReportsModel = new JSONModel(JSON.parse(drdata));
+					_this.getView().setModel(directReportsModel, "DirectReportsModel");
+				},
+				error: function(){
+				    alert("failed to get direct reports");
+				}
+			});
+        },
+        
+        bindMockData: function(){
+            var directReportsModelPath = jQuery.sap.getModulePath("hcpsuccessfactors", "/mockData/directReports.json");
+			var directReportsModel = new JSONModel();
+			directReportsModel.loadData(directReportsModelPath, null, false);
+			this.getView().setModel(directReportsModel, "directReportsModel");
+        }
 	});
 
 });
