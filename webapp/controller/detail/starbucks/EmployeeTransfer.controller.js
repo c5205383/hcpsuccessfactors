a561d3937fd9bfb2d2933e8b9d149e23107cc61b
Lily Li <lily.li11@sap.com>
2016-05-03 08:01:47 +0000

    lily0503

--- webapp/controller/detail/starbucks/EmployeeTransfer.controller.js
+++ webapp/controller/detail/starbucks/EmployeeTransfer.controller.js
@@ -3,10 +3,13 @@ sap.ui.define([
 	"jquery.sap.global",
 	"sap/m/Button",
 	"sap/m/MessageToast",
+	"sap/ui/model/Filter",
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel"
-], function(BaseController,jQuery, Button, MessageToast, Controller, JSONModel) {
+], function(BaseController, jQuery, Button, MessageToast, Filter, Controller, JSONModel) {
 	"use strict";
+
+	var oItem = null;
 
 	return BaseController.extend("hcpsuccessfactors.controller.detail.starbucks.EmployeeTransfer", {
 
@@ -18,9 +21,7 @@ sap.ui.define([
 			} else {
 				this.bindMockData();
 			}
-			var oView = this.getView();
-			oView.setModel(this.oModel);
-			this.oSF = oView.byId("searchField");
+			this.oSF = this.byId("searchField");
 			this.onButtonPressed();
 		},
 
@@ -76,10 +77,88 @@ sap.ui.define([
 				oLayout.setVisible(true);
 			}
 		},
+		OnAddPress: function(event) {
+			//	var username = this.byId("searchField").getValue();
+			var oCtx = oItem.getBindingContext("DirectReportsModel");
+			var oTable = this.byId("transfertable");
+			var myDate = new Date();
+			var Effective = myDate.getFullYear() + "/" + myDate.getMonth() + "/" + myDate.getDay() + " " + myDate.getHours() + ":" + myDate.getMinutes() +
+				":" + myDate.getSeconds();
+
+			var itemList = new sap.m.ColumnListItem({
+				//type : Active
+				//press:onItemPress
+			});
+			itemList.addCell(
+				new sap.m.Text({
+					text: oCtx.getProperty("empId")
+				})
+			);
+			itemList.addCell(
+				new sap.m.Text({
+					text: oCtx.getProperty("defaultFullName")
+				})
+			);
+			itemList.addCell(
+				new sap.m.Text({
+					text: null
+				}));
+			itemList.addCell(
+				new sap.m.Text({
+					text: oCtx.getProperty("location")
+				}));
+			itemList.addCell(
+				new sap.m.Text({
+					text: oCtx.getProperty("defaultLocale")
+				}));
+			itemList.addCell(
+				new sap.m.Text({
+					text: this.byId("transcity").getSelectedItem().getText()
+				}));
+			itemList.addCell(
+				new sap.m.Text({
+					text: this.byId("transbranch").getSelectedItem().getText()
+				}));
+			itemList.addCell(
+				new sap.m.Text({
+					text: Effective
+				})
+			);
+			oTable.addItem(itemList);
+			/*	var myDate = new Date();
+				var Effective = myDate.getFullYear()+"/"+myDate.getMonth()+"/"+myDate.getDay()+" "+ myDate.getHours()+":"+myDate.getMinutes()+":"+myDate.getSeconds();
+				var data = JSON.stringify({
+							empId : oCtx.getProperty("empId"),
+							defaultFullName : oCtx.getProperty("defaultFullName"),
+							position : null,
+							location : oCtx.getProperty("location"),
+							defaultLocale : oCtx.getProperty("defaultLocale"),
+							transcity : this.byId("transcity").getSelectedItem().getText(),
+							transbranch : this.byId("transbranch").getSelectedItem().getText(),
+							effectiveDate : Effective
+						});
+				var oModelIn = new sap.ui.model.json.JSONModel();
+					 oModelIn.setData(data);
+					 oModelIn.setDefaultBindingMode("OneWay");
+					 oTable.setModel(oModelIn);
+					// oTable.unbindRows().bindRows("/");*/
+		},
+
 		onSearch: function(event) {
 			var item = event.getParameter("suggestionItem");
 			if (item) {
 				sap.m.MessageToast.show("search for: " + item.getText());
+				//var oCtx = item.getBindingContext("DirectReportsModel");
+				oItem = item;
+				//oCtx.getProperty("empId");
+				/*var data = JSON.stringify({
+						empId : oCtx.getProperty("empId"),
+						defaultFullName : oCtx.getProperty("defaultFullName"),
+						username : oCtx.getProperty("username"),
+						location : oCtx.getProperty("location"),
+						defaultLocale : oCtx.getProperty("defaultLocale")
+					});*/
+				// this.byId("transfertable").addItem(oItem);
 			}
 		},
 
@@ -91,13 +170,14 @@ sap.ui.define([
 					new sap.ui.model.Filter("userId", function(sText) {
 						return (sText || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
 					}),
-					new sap.ui.model.Filter("username", function(sDes) {
+					new sap.ui.model.Filter("defaultFullName", function(sDes) {
 						return (sDes || "").toUpperCase().indexOf(value.toUpperCase()) > -1;
 					})
 				], false)];
 			}
 
-			this.oSF.getBinding("suggestionItems").filter(filters);
+			var binding = this.oSF.getBinding("suggestionItems");
+			binding.filter(filters);
 			this.oSF.suggest();
 		}
 
